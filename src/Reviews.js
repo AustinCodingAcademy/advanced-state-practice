5c4ccaa4eb98b079c4b26483396cb70904b18a2b
saf0911 <saf0911@gmail.com>
2017-05-10 18:42:58 -0500

    just getting started

---
+++ src/Reviews.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+
+export default class Reviews extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    function review
+  }
+}
